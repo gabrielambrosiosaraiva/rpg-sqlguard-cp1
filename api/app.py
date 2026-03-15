80da77bb9f8e6ffac9f5d4756daaee54e709d4e4
gabrielambrosiosaraiva <saraiva.gabrielambrosio@gmail.com>
2026-03-15 18:30:51 -0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -3,6 +3,11 @@ import oracledb, os
 
 app = FastAPI()
 
+DB_USER = os.environ["DB_USER"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+DB_DSN = os.environ["DB_DSN"]
+
+
 pool = oracledb.create_pool(
     user=DB_USER,
     password=DB_PASSWORD,
